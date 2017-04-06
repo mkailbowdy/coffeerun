da715139bd07f843dea0207165cddddae2a45784
Myhkail Mendoza <myhkail.mendoza@gmail.com>
2017-04-05 21:17:26 -0700

    Finish DataStore.js module. DataStore stores email addresses and coffee order in an object.

---
+++ scripts/datastore.js
@@ -0,0 +1,36 @@
+// The datastore.js module will store emails and the cost of their purchase. it can also remove those entries. it also provides the ability to see the list of emails and their values
+(function(window) {
+    'use strict';
+    var App = window.App || {};
+
+    // DataStore(), using capital letters for the words is a convention when naming constructors.
+    function DataStore() {
+
+        this.data = {}; // data is a instance variable that holds key-value pairing.
+    }
+
+    // prototypes are pretty much class variables and methods. available to all instances.
+    // Here we give DataStore.prototype the 'add' property. 'add' will be a function.
+    DataStore.prototype.add = function(key, val) {
+        // key is the customer's email address, val is the cost of the coffee.
+        this.data[key] = val;
+    };
+
+    DataStore.prototype.get = function(key) {
+        return this.data[key];
+    };
+
+    DataStore.prototype.getAll = function() {
+        return this.data;
+    }
+
+    DataStore.prototype.remove = function(key) {
+        delete this.data[key]; // Delete operator removes a key/value pair from an object.
+
+    }
+
+
+    App.DataStore = DataStore; // Attaches DataStore function to App
+    window.App = App; // Reassigns App to the window.
+
+})(window);
